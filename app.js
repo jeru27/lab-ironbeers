59f76a9bda2ce9c9acc94482bc24e965c847a367
jeru27 <jrumora@gmail.com>
2021-12-08 16:25:34 -0800

    commit

--- app.js
+++ app.js
@@ -21,5 +21,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(listOfBeers => {
+
+  res.render('beers', {listOfBeers: listOfBeers})
+    //res.render ('first argument is the path to get to what you want to render', {second argument has to be an object, give it a key and a value, use curly braces})
+})
+
+.catch(error => {
+  console.log(error)
+})
+
+
+});
+
+app.get('/random-beers', (req, res, next) => {
+punkAPI.getRandom()
+.then(theRandomBeer => {
+
+  res.render('random', {oneBeer: theRandomBeer})
+})
+ .catch(error => {
+  console.log(error)
+})
+
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
